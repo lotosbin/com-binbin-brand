511de8af18aa1eae9fac0ab9929c620089313444
lotosbin <lotosbin@gmail.com>
2011-07-18 18:26:11 +0800

    test datastore service

--- src/com/binbin/brand/BrandEntity.java
+++ src/com/binbin/brand/BrandEntity.java
@@ -3,7 +3,6 @@ package com.binbin.brand;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.codelab.Util;
 
 public class BrandEntity {
 	public static Entity getProduct(String name) {
